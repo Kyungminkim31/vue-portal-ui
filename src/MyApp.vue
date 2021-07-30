59450bf68b67adf344bedcfecf1ba8c1b9ec0be5
kyungminkim31 <M31413141m!>
2021-07-30 12:16:32 +0900

    MyApp 추가, 테마스위치, alert 샘플, 버튼 샘플 추가

---
+++ src/MyApp.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+    <v-main>
+      <v-container>
+        <h3> a Switch for Theme </h3>
+        <div>
+          <v-switch
+            v-model="$vuetify.theme.dark"
+            hint="전체적인 테마를 바꾸는 스위치"
+            inset
+            label="Vuetify Theme Dark"
+            persistent-hint
+          ></v-switch>
+        </div>
+        <h3> Alerts Sample</h3>
+        <v-alert
+          icon="mdi-firework"
+          dense
+          type="primary"
+        >primary</v-alert>
+        <v-alert
+          dense
+          type="secondary"
+        >secondary</v-alert>
+        <v-alert
+          dense
+          type="warning"
+        >warning</v-alert>
+
+        <h3> Buttons Sample </h3>
+        <v-btn
+          color="primary"
+          >Primary</v-btn>
+        <v-btn
+          color="secondary"
+          >secondary</v-btn>
+        <v-btn
+          color="error"
+          >secondary</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<style>
+h3 {
+  margin : 24px  0 10px 0 ;
+}
+</style>
+<script>
+export default {
+  name: 'MyApp',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import MyApp from './MyApp.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  //render: h => h(App)
+  render: h => h(MyApp)
 }).$mount('#app')
